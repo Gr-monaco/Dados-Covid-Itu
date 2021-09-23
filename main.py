529660152f6ce1d21ce1b388fe737ba8026249c6
Gabriel Monaco <gr-monaco@hotmail.com>
2021-09-23 19:21:37 -0300

    Adicionar função generica para buscar imagem

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import shutil
-
+import time
 import requests
+import random
 from bs4 import BeautifulSoup
-
-
 
 headers = {
     'Access-Control-Allow-Origin': '*',
@@ -13,14 +12,31 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
     }
 
-url = "https://itu.sp.gov.br/boletim-coronavirus-itu-16/"
 
-req = requests.get(url,headers)
-soup = BeautifulSoup(req.content, 'html.parser')
-imagens = soup.find(class_="mkdf-post-image").find('img')
-print(imagens.attrs['src'])
-r = requests.get(imagens.attrs['src'],headers, stream=True)
+def pegar_imagem(url, index):
+    print(f'{index} url: {url}')
+    req = requests.get(url, headers)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    imagens = soup.find(class_="mkdf-post-image").find('img')
+    print(imagens.attrs['src'])
+    r = requests.get(imagens.attrs['src'], headers, stream=True)
 
-with open("imagens/image.jpeg", "wb") as f:
-    r.raw.decode_contet = True
-    shutil.copyfileobj(r.raw, f)
+    pasta = "imagens/imagem" + str(index) + ".jpeg"
+    with open(pasta, "wb") as f:
+        r.raw.decode_contet = True
+        shutil.copyfileobj(r.raw, f)
+
+    timer = random.randint(5, 10)
+    print(f'Esperando {timer} segundos antes de ir para proximo URL')
+    time.sleep(timer)
+    print()
+    
+
+url_incompleto = "https://itu.sp.gov.br/boletim-coronavirus-itu-"
+
+for i in range(16, 29):
+    url_completo = url_incompleto + str(i) + "/"
+    pegar_imagem(url_completo, i)
+
+
+
