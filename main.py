006dc396eb302a1672ce1483fe05e350890fac8c
Gabriel Monaco <gr-monaco@hotmail.com>
2021-09-23 19:58:43 -0300

    Adicionar validador de pagina existente

--- main.py
+++ main.py
@@ -16,25 +16,27 @@ headers = {
 def pegar_imagem(url, index):
     print(f'{index} url: {url}')
     req = requests.get(url, headers)
-    soup = BeautifulSoup(req.content, 'html.parser')
-    imagens = soup.find(class_="mkdf-post-image").find('img')
-    print(imagens.attrs['src'])
-    r = requests.get(imagens.attrs['src'], headers, stream=True)
+    if req.status_code == 200:
+        soup = BeautifulSoup(req.content, 'html.parser')
+        imagens = soup.find(class_="mkdf-post-image").find('img')
+        r = requests.get(imagens.attrs['src'], headers, stream=True)
 
-    pasta = "imagens/imagem" + str(index) + ".jpeg"
-    with open(pasta, "wb") as f:
-        r.raw.decode_contet = True
-        shutil.copyfileobj(r.raw, f)
+        pasta = "imagens/imagem" + str(index) + ".jpeg"
+        with open(pasta, "wb") as f:
+            r.raw.decode_contet = True
+            shutil.copyfileobj(r.raw, f)
 
-    timer = random.randint(5, 10)
-    print(f'Esperando {timer} segundos antes de ir para proximo URL')
-    time.sleep(timer)
-    print()
-    
+        timer = random.randint(10, 30)
+        print(f'Esperando {timer} segundos antes de ir para proximo URL')
+        time.sleep(timer)
+        print()
+    else:
+        print("Página com erro!")
+
 
 url_incompleto = "https://itu.sp.gov.br/boletim-coronavirus-itu-"
 
-for i in range(16, 29):
+for i in range(92, 383):
     url_completo = url_incompleto + str(i) + "/"
     pegar_imagem(url_completo, i)
 
