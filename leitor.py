946c9b19ee14ef7ef86b7367dc27ead083454cdb
Gabriel Monaco <gr-monaco@hotmail.com>
2021-09-28 12:57:06 -0300

    Adicionar funcionalidade básica de leitura
    O leitor ainda é bem cru, tem problemas na hora de ler zeros depois de numeros, tipo 10 ou 20.
    O leitor só adiciona numeros e porcentagem, então já pode ser convertido direto para float ou int.
    Os dados com erros podem ser adicionados na revisão.

---
+++ leitor.py
@@ -0,0 +1,28 @@
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+import cv2  
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+
+
+f = open('saidas.txt', 'w')
+
+for i in range(16, 72):
+    print('Imagem ', str(i))
+    original = cv2.imread('ImagemAlteradas/imagem' + str(i) +'.jpeg')
+    original = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+    ret, original2 = cv2.threshold(original, 100, 255, cv2.THRESH_BINARY)
+    # scale_percent = 200  # percent of original size
+    # width = int(original2.shape[1] * 300 / 100)
+    # height = int(original2.shape[0] * scale_percent / 100)
+    # dim = (width, height)
+    # original2 = cv2.resize(original2, dim, interpolation=cv2.INTER_AREA)
+    recorte = original2[250:350, 690:980]
+    leitura = pytesseract.image_to_string(recorte, config="-c tessedit_char_whitelist=0123456789% --psm 13 --oem 3  ")
+    print(leitura)
+    limpo = ''.join(i for i in leitura if i.isdigit())
+    f.write(str(i) + ' ' + limpo + '\n')
+
+f.close()
