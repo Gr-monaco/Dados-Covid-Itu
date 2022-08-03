9f132d3fec5944e856556655518876ed8004446d
Gr-monaco <gr-monaco@hotmail.com>
2022-08-03 19:20:28 -0300

    Insere try catch para ambientes sem git ignore

--- mudartamanh.py
+++ mudartamanh.py
@@ -2,15 +2,19 @@ import cv2 as cv
 import os
 
 lista = os.listdir('imagens')
-lista.remove('.gitignore')
 
-for i in lista:
-    imagem_original = cv.imread('imagens/'+i)
-    width, height, channels = imagem_original.shape
-    print(imagem_original.shape)
-    nova_imagem = imagem_original.copy()
+try:
+    lista.remove('.gitignore')
+except ValueError:
+    print("GitIgnore não achado, continuando")
+finally:
+    for i in lista:
+        imagem_original = cv.imread('imagens/'+i)
+        width, height, channels = imagem_original.shape
+        print(imagem_original.shape)
+        nova_imagem = imagem_original.copy()
 
-    if height == 1300 and width == 800:
-        nova_imagem = cv.resize(nova_imagem, (1280, 787))
+        if height == 1300 and width == 800:
+            nova_imagem = cv.resize(nova_imagem, (1280, 787))
 
-    cv.imwrite('ImagemAlteradas/'+str(i) , nova_imagem)
+        cv.imwrite('C:/Users/gr-mo/PycharmProjects/Dados-Covid-Itu/ImagensAlteradas/'+str(i) , nova_imagem)
