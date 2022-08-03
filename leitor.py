d0a04771a4c7b6b2c5a9e2878f1adb2345eee146
Gr-monaco <gr-monaco@hotmail.com>
2022-08-03 20:16:22 -0300

    Cria segmento de leitura de data

--- leitor.py
+++ leitor.py
@@ -3,7 +3,9 @@ try:
 except ImportError:
     import Image
 import pytesseract
-import cv2  
+import cv2
+
+# https://muthu.co/all-tesseract-ocr-options/ <- Link para todos os parametros de tesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
 
@@ -19,10 +21,20 @@ for i in range(16, 72):
     # height = int(original2.shape[0] * scale_percent / 100)
     # dim = (width, height)
     # original2 = cv2.resize(original2, dim, interpolation=cv2.INTER_AREA)
-    recorte = original2[250:350, 690:980]
-    leitura = pytesseract.image_to_string(recorte, config="-c tessedit_char_whitelist=0123456789% --psm 13 --oem 3  ")
-    print(leitura)
-    limpo = ''.join(i for i in leitura if i.isdigit())
+    casos_descartados = original2[250:350, 690:980]
+    dia_do_boletim = original2[50:80, 1000:1250]
+    leitura_de_casos_descartados = pytesseract.image_to_string(casos_descartados,
+                                                               config="-c tessedit_char_whitelist=0123456789% "
+                                                                      "--psm 13 --oem 3  ")
+    # https://github.com/tesseract-ocr/tesseract/issues/2923#issuecomment-598503707 <- Resolveu o problema de não
+    #                                                                                  detectar espaços.
+    leitura_do_dia = pytesseract.image_to_string(dia_do_boletim,
+                                                               config="-c tessedit_char_whitelist="
+                                                                      "'abcdefghijklmnopqrstuvwxyz '0123456789 "
+                                                                      "--psm 13 --oem 3  ")
+    print(leitura_do_dia)
+    print(leitura_de_casos_descartados)
+    limpo = ''.join(i for i in leitura_de_casos_descartados if i.isdigit())
     f.write(str(i) + ' ' + limpo + '\n')
 
 f.close()
