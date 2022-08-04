03ed1e24bd586cf46f071ccf1246afef01ae90b6
Gr-monaco <gr-monaco@hotmail.com>
2022-08-04 19:25:02 -0300

    - Adição de leitura de casos descartados e obitos confirmados. - Menção da fonte para tesseract utilizar o training data correto em CONFIG_NUMERO

--- leitor.py
+++ leitor.py
@@ -6,10 +6,29 @@ import pytesseract
 import cv2
 
 # https://muthu.co/all-tesseract-ocr-options/ <- Link para todos os parametros de tesseract
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
 
-CONFIG_NUMERO = "-c tessedit_char_whitelist=0123456789 --psm 13 --oem 3"
+CONFIG_NUMERO = "-l lato --psm 13 --oem 3 -c tessedit_char_whitelist=0123456789"
 CONFIG_TEXTO = "-c tessedit_char_whitelist='abcdefghijklmnopqrstuvwxyz '0123456789 --psm 13 --oem 3 "
+
+
+def leitura_de_casos_descartados(numero, imagem):
+    recorte = imagem[250:350, 690:980]
+    if 16 <= numero < 72:
+        recorte = imagem[250:350, 690:980]
+
+    retorno = pytesseract.image_to_string(recorte, config=CONFIG_NUMERO).rstrip()
+
+    return retorno
+
+def leitura_de_obitos_confirmados(numero, imagem):
+    recorte = imagem[610:715, 690:860]
+    if 16 <= numero < 72:
+        recorte = imagem[610:715, 690:860]
+
+    retorno = pytesseract.image_to_string(recorte, config=CONFIG_NUMERO).rstrip()
+
+    return retorno
 
 def leitura_de_casos_confirmados(numero, imagem):
     """
@@ -25,7 +44,7 @@ def leitura_de_casos_confirmados(numero, imagem):
     if 16 <= numero <= 38:
         recorte = imagem[250:350, 160:330]
     if 38 < numero <= 72:
-        recorte = imagem[250:297, 140:242]
+        recorte = imagem[250:297, 150:242]
 
     retorno = pytesseract.image_to_string(recorte, config=CONFIG_NUMERO).rstrip()
 
@@ -59,17 +78,16 @@ for i in range(16, 72):
     dia_do_boletim = original2[50:80, 1000:1250]
 
     # https://docs.python.org/3/library/stdtypes.html#str.rstrip
-    leitura_de_casos_descartados = pytesseract.image_to_string(casos_descartados,
-                                                               config="-c tessedit_char_whitelist=0123456789 "
-                                                                      "--psm 13 --oem 3  ").rstrip()
+    casos_descartados_n = leitura_de_casos_descartados(i, original2)
     # https://github.com/tesseract-ocr/tesseract/issues/2923#issuecomment-598503707 <- Resolveu o problema de não
     #                                                                                  detectar espaços.
     leitura_do_dia = leitura_de_data(i, original2)
     casos_confirmados = leitura_de_casos_confirmados(i, original2)
+    obitos_confirmados = leitura_de_obitos_confirmados(i, original2)
     print(leitura_do_dia)
     print(casos_confirmados)
-    print(leitura_de_casos_descartados)
-    limpo = ''.join(i for i in leitura_de_casos_descartados if i.isdigit())
-    f.write(str(i) + ' ' + leitura_do_dia.rstrip() + ' ' + limpo + ' ' + casos_confirmados + '\n')
+    print(obitos_confirmados)
+    print("casos des" + casos_descartados_n)
+    f.write(str(i) + ' ' + leitura_do_dia.rstrip() + ' ' + casos_descartados_n + ' ' + str(casos_confirmados) + ' ' + obitos_confirmados + '\n')
 
 f.close()
