37e85b2f10641d00e9b1b9803c5bfc93afc27eb2
Gr-monaco <gr-monaco@hotmail.com>
2022-08-13 13:23:04 -0300

    Remove linhas

--- leitor.py
+++ leitor.py
@@ -80,58 +80,6 @@ def leitura(numero: int, imagem, parametros: Parametro):
     pass
 
 
-def leitura_de_casos_descartados(numero, imagem):
-    recorte = imagem[250:350, 700:985]  # funciona bem para 4 digitos
-
-    contours, hierarchy = cv2.findContours(recorte, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
-    stencil = np.zeros(recorte.shape).astype(imagem.dtype)
-    ROI = []
-    for con in contours:
-        if 300 <= cv2.contourArea(con):
-            ROI.append(con)  # ROI
-
-    for con in ROI:
-        x, y, w, h = cv2.boundingRect(con)
-        roi = recorte[y:y + h, x:x + w]
-
-        stencil[y:y + h, x:x + w] = roi
-
-    retorno = pytesseract.image_to_string(stencil, config=CONFIG_NUMERO).rstrip()
-
-    return retorno
-
-
-def leitura_de_obitos_confirmados(numero, imagem):
-    recorte = imagem[610:715, 690:860]
-    if 16 <= numero < 72:
-        recorte = imagem[610:715, 690:860]
-
-    retorno = pytesseract.image_to_string(recorte, config=CONFIG_NUMERO).rstrip()
-
-    return retorno
-
-
-def leitura_de_casos_confirmados(numero, imagem):
-    """
-    Faz a leitura do numero de casos confirmados.
-    Dependendo do numero da imagem, a leitura é feita em um trecho diferente. Este comportamento
-    ainda é hardcoded.
-    @rtype: int
-    @param numero: numero da imagem sendo analizada
-    @param imagem: imagem a ser analizada
-    """
-    recorte = imagem[250:350, 160:330]  # valor padrão
-    if 16 <= numero <= 38:
-        recorte = imagem[250:350, 160:330]
-    if 38 < numero <= 72:
-        recorte = imagem[250:297, 150:242]
-
-    retorno = pytesseract.image_to_string(recorte, config=CONFIG_NUMERO).rstrip()
-
-    return retorno
-
-
 def leitura_de_data(numero, imagem):
     recorte = imagem[50:80, 1000:1250]  # valor padrão
     if 16 <= numero <= 48:
@@ -174,17 +122,8 @@ for i in range(16, 502):
     original = cv2.imread('C:/Users/gr-mo/PycharmProjects/Dados-Covid-Itu/ImagensAlteradas/imagem' + str(i) + '.jpeg')
     original = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
     ret, original2 = cv2.threshold(original, 170, 255, cv2.THRESH_BINARY)
-    # scale_percent = 200  # percent of original size
-
-    # width = int(original2.shape[1] * 300 / 100)
-    # height = int(original2.shape[0] * scale_percent / 100)
-    # dim = (width, height)
-    # original2 = cv2.resize(original2, dim, interpolation=cv2.INTER_AREA)
-    casos_descartados = original2[250:350, 690:980]
-    dia_do_boletim = original2[50:80, 1000:1250]
 
     leitura_do_dia = leitura_de_data(i, original2).replace(" ", "")
-    print("RAW: ", leitura_do_dia)
     leitura_do_dia = leitura_do_dia[0:2] + '/' + acha_mes(leitura_do_dia) + '/' + leitura_do_dia[-4:]
     casos_confirmados = leitura(i, original2, params_casos_conf)
     coluna_numero_imagem = np.append(coluna_numero_imagem, i)
